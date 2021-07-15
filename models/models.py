c5e22ae1a2be3d97ad4d53b3f4e504bbcc804eea
luissancar <luissancar@yahoo.es>
2021-07-15 16:53:54 +0200

    primero

--- models/models.py
+++ models/models.py
@@ -20,4 +20,30 @@
 
 
 
-jhhhhh
+
+from odoo import models, fields, api
+
+class alumno(models.Model):
+	_name = 'relaciones.alumno'
+	_description = 'model alumno'
+
+	name = fields.Char('DNI',required=True)
+	nombre = fields.Char(string='Nombre',required=True)
+
+
+
+class asignatura(models.Model):
+	_name = 'relaciones.asignatura'
+	_description = 'model asignatura'
+
+	name = fields.Char('Código',required=True)
+	nombre = fields.Char(string='Asignatura',required=True)
+
+
+
+class tutor(models.Model):
+	_name = 'relaciones.tutor'
+	_description = 'model tutor'
+
+	name = fields.Char('DNI',required=True)
+	nombre = fields.Char(string='Tutor',required=True)
